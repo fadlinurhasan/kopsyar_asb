6fb54e0f95ffe947f5998e604325b84ec3f7c9de
Fadli Nurhasan <fadli@starqle.com>
2017-02-24 06:57:57 +0700

    master #comment ADD: crud users

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   resources :simpanan_histories
   resources :tipe_simpanans
   devise_for :users
+
   root to: 'home#index'
+  resources :users, path: 'admin/users'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
